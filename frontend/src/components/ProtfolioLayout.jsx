af129b11a9011638025d3a12f3b1ef0011c8ed12
CatDeveloper <maks.rozhkov.2004@gmail.com>
2023-03-15 23:02:24 +0500

    fix: delete footer

--- frontend/src/components/ProtfolioLayout.jsx
+++ frontend/src/components/ProtfolioLayout.jsx
@@ -11,7 +11,7 @@ const PortfolioLayout = () => {
             <MainContent>
                 <Outlet/>
             </MainContent>
-            <Footer/>
+            {/*<Footer/>*/}
         </div>
     );
 };
